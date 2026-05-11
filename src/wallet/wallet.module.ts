51112d1f717dcb7857867eb8bab9ce850aa793c7
carlkanaan <ckanaan02@gmail.com>
2026-05-11 13:12:20 +0300

    feat(wallet): add withdrawal approval workflow

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -4,10 +4,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 import { Wallet, WalletSchema } from './schemas/wallet.schema';
+import {
+  WithdrawalRequest,
+  WithdrawalRequestSchema,
+} from './schemas/withdrawal-request.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Wallet.name, schema: WalletSchema }]),
+    MongooseModule.forFeature([
+      { name: Wallet.name, schema: WalletSchema },
+      {
+        name: WithdrawalRequest.name,
+        schema: WithdrawalRequestSchema,
+      },
+    ]),
   ],
   controllers: [WalletController],
   providers: [WalletService],
